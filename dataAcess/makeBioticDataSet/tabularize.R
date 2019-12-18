1afc22c9a50a33e4b12995042f57ba24aea32fdf
edvinf <edvin.fuglebakk@gmail.com>
2019-12-18 02:01:05 +0100

    Suggestion for how to provide customized views of biotic data

---
+++ dataAcess/makeBioticDataSet/tabularize.R
@@ -0,0 +1,128 @@
+library(data.table)
+
+#' @noRd
+mergeTable <- function(flatBiotic, table, columns, keys, prune=F){
+  
+  if (any(columns %in% names(table))){
+    if (is.null(flatBiotic)){
+      return(table[,unique(c(keys, columns[columns %in% names(table)])), with=F])
+    }
+    else{
+      return(merge(table[,unique(c(keys, columns[columns %in% names(table)])), with=F], flatBiotic, by=keys, all.x=!prune))
+    }
+  }
+  if (!is.null(flatBiotic)){
+    return(merge(table[,keys, with=F], flatBiotic, by=keys))    
+  }
+  return(NULL)
+}
+
+#' Make tabular view of biotic data
+#' @description 
+#'  produces a single 2D table with biotic data.
+#' @details
+#'  the columns that should be included in the view is provided in the argument 'columns'.
+#'  (field names are unqiue across levels in biotic 3)
+#'  Key columns are autmatically included. Do not specify these.
+#'  Only levels implied by these columns are included.
+#' 
+#'  relationalBiotic could be obtained by RstoxData::readXmlFile, or any other parser
+#'  that produces the following format:
+#'  relationalBiotic should be a named list containing data.tables.
+#'  each table should be named as corresponding elements in biotic 3 (namespace: http://www.imr.no/formats/nmdbiotic/v3)
+#'  all columns should be named as corresponding elements and attributes in biotic 3.
+#'  
+#'  age parameters are considered part of the individual level,
+#'  and views will be constrainted to one age reading for each individual,
+#'  although biotic3 supports several.
+#'  
+#'  views does not allow columns from different branches of the hiearchy in the same view
+#'  (apart from age reading, which is constrained to only one per individual).
+#'  E.g. can not include columns from both prey and tag, or from both preylengthfrequencytable and copepodedevstagefrequencytable
+#' @param relationalBiotic list() of tabular representation of the different levels in biotic
+#' @param columns name of columns to keep (in addition to key columns)
+#' @param prune logical, if TRUE, whether to prune results
+#' @return data.table with the requested columns, in addition to any key columns.
+makeTabularView <- function(relationalBiotic, columns, prune=F){
+  
+  allColumnNames <- c(names(relationalBiotic$agedetermination), 
+                      names(relationalBiotic$catchsample), 
+                      names(relationalBiotic$copepodedevstagefrequencytable),
+                      names(relationalBiotic$fishstation),
+                      names(relationalBiotic$individual),
+                      names(relationalBiotic$mission),
+                      names(relationalBiotic$prey),
+                      names(relationalBiotic$preylengthfrequencytable),
+                      names(relationalBiotic$tag))
+  
+  if (!all(columns %in% allColumnNames)){
+    stop("Some specified columns does not exist i format: ", paste(columns[!(columns %in% allColumnNames)], collapse=","))
+  }
+  
+  #biotic 3 key structure
+  missionkeys <- c("missiontype", "startyear", "platform", "missionnumber")
+  stationkeys <- c(missionkeys, "serialnumber")
+  catchkeys <- c(stationkeys, "catchsampleid")
+  individualkeys <- c(catchkeys, "specimenid")
+  agedetkeys <- c(individualkeys, "agedeterminationid")
+  tagkeys <- c(individualkeys, "tagid")
+  preykeys <- c(individualkeys, "preysampleid")
+  preylengthfrequencykeys <- c(preykeys, "preylengthid")
+  copepodedevstagefrequencykeys <- c(preykeys, "copepodedevstage")
+  
+  #keys are automatically added where appropriate.
+  columns <- columns[!(columns %in% c(agedetkeys, tagkeys, preylengthfrequencykeys, copepodedevstagefrequencykeys))]
+  
+  if (any(columns %in% names(relationalBiotic$agedetermination))){
+    #set prefferred age reading where missing
+    ageids <- unique(relationalBiotic$agedetermination[,agedetkeys, with=F])
+    ageids <- ageids[!duplicated(ageids[,individualkeys, with=F])]
+    relationalBiotic$individual <- merge(relationalBiotic$individual, ageids, by=individualkeys, all.x=T)
+    relationalBiotic$individual[is.na(relationalBiotic$individual$preferredagereading),"preferredagereading"] <- as.integer(relationalBiotic$individual[is.na(relationalBiotic$individual$preferredagereading),][["agedeterminationid"]])
+    relationalBiotic$individual$agedeterminationid <- NULL
+  }
+  
+  if (any(columns %in% names(relationalBiotic$tag))){
+    if (any(columns %in% names(relationalBiotic$copepodedevstagefrequencytable)) |
+        any(columns %in% names(relationalBiotic$preylengthfrequencytable)) |
+        any(columns %in% names(relationalBiotic$prey))){
+      stop("Cannot view several branches of the hiearchy in one view. Columns from tag, may not be comibined with prey-related columns.")
+    }
+  }
+  
+  if (any(columns %in% names(relationalBiotic$copepodedevstagefrequencytable)) &
+      any(columns %in% names(relationalBiotic$preylengthfrequencytable))){
+    stop("Cannot view several branches of the hiearchy in one view. columns from copepodedevstagefrequencytable, may not be comibined with columns from preylengthfrequencytable")
+  }
+  
+  # These can only occur as leaf tables
+  flatbiotic <- NULL
+  flatbiotic <- mergeTable(flatbiotic, relationalBiotic$tag, columns, tagkeys, prune)
+  flatbiotic <- mergeTable(flatbiotic, relationalBiotic$copepodedevstagefrequencytable, columns, copepodedevstagefrequencykeys, prune)
+  flatbiotic <- mergeTable(flatbiotic, relationalBiotic$preylengthfrequencytable, columns, preylengthfrequencykeys, prune)
+  flatbiotic <- mergeTable(flatbiotic, relationalBiotic$prey, columns, preykeys, prune)
+  if (!any(columns %in% names(relationalBiotic$agedetermination))){
+    flatbiotic <- mergeTable(flatbiotic, relationalBiotic$individual, columns, individualkeys, prune)
+  }
+  else if (any(columns %in% names(relationalBiotic$agedetermination))){
+    agedet <- mergeTable(NULL, relationalBiotic$agedetermination, columns, agedetkeys)
+    ind <- relationalBiotic$individual
+    ind$agedeterminationid <- ind$preferredagereading
+    ind <- merge(relationalBiotic$individual, agedet, all.x=T)
+    flatbiotic <- mergeTable(flatbiotic, ind, columns, agedetkeys, prune)
+  }
+  else{
+    stop()
+  }
+  
+  flatbiotic <- mergeTable(flatbiotic, relationalBiotic$catchsample, columns, catchkeys, prune)
+  flatbiotic <- mergeTable(flatbiotic, relationalBiotic$fishstation, columns, stationkeys, prune)
+  flatbiotic <- mergeTable(flatbiotic, relationalBiotic$mission, columns, missionkeys, prune)
+
+  return(flatbiotic)
+}
+
+#' Custom view of individuals, with common additions from catch level and age reading level
+individualView <- function(relationalBiotic, additional=c("catchcategory", "catchpartnumber", "commonname", "scientificname", "age", "otolithtype")){
+  return(makeTabularView(relationalBiotic, c(names(relationalBiotic$individual), additional), prune=T))
+}
