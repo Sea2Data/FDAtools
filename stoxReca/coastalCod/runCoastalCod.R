a591c306147973e17d9f733bb5aceb6c2a19826f
edvinf <edvin.fuglebakk@gmail.com>
2020-03-30 14:09:58 +0200

    minor updates

--- stoxReca/coastalCod/runCoastalCod.R
+++ stoxReca/coastalCod/runCoastalCod.R
@@ -9,33 +9,20 @@
 # - redistribute landings based on logbook records.
 # - run or re-run the estimate with this modified configuration.
 
-# run logbook cleaning 
 
 library(Rstox)
 
-#' Assigns the given covariate in landings based on the following rules:
-#' hovedområdekode==0  --> 000
-#' hovedområdekode==3 & kysthavkode==0 --> 300
-#' hovedområdekode==3 & kysthavkode==1 --> 301
-#' ...
-#' hovedområdekode==7 & kysthavkode==0 --> 700
-#' hovedområdekode==7 & kysthavkode==1 --> 701
-#' 
+#' Assigns the given covariate in landings based mainarea and terretorial-line codings (hovedområde & kyst/hav)
 #' @param x data.frame with landings
 #' @param covariate identifies a column in x
 assignSpatialLandingsCoastalCod <- function(x, covariate="spatial"){
   # 8 kyst, 0 hav
-  x[x$hovedområdekode == 0, covariate] <- "s000"
-  x[x$hovedområdekode == 3 & x$kysthavkode == 0,covariate] <- "s300"
-  x[x$hovedområdekode == 3 & x$kysthavkode == 8,covariate] <- "s301"
-  x[x$hovedområdekode == 4 & x$kysthavkode == 0,covariate] <- "s400"
-  x[x$hovedområdekode == 4 & x$kysthavkode == 8,covariate] <- "s401"
+  x[x$hovedområdekode %in% c(3,4) & x$kysthavkode == 0,covariate] <- "s300-400"
+  x[x$hovedområdekode %in% c(3,4) & x$kysthavkode == 8,covariate] <- "s301-401"
   x[x$hovedområdekode == 5 & x$kysthavkode == 0,covariate] <- "s500"
-  x[x$hovedområdekode == 5 & x$kysthavkode == 8,covariate] <- "s501"
-  x[x$hovedområdekode == 6 & x$kysthavkode == 0,covariate] <- "s600"
-  x[x$hovedområdekode == 6 & x$kysthavkode == 8,covariate] <- "s601"
-  x[x$hovedområdekode == 7 & x$kysthavkode == 0,covariate] <- "s700"
-  x[x$hovedområdekode == 7 & x$kysthavkode == 8,covariate] <- "s701"
+  x[x$hovedområdekode == 0 | x$hovedområdekode == 5 & x$kysthavkode == 8,covariate] <- "s000-501"
+  x[x$hovedområdekode %in% c(6,7) & x$kysthavkode == 8,covariate] <- "s601-701"
+  x[x$hovedområdekode %in% c(6,7) & x$kysthavkode == 0,covariate] <- "s600-700"
   if (!all(x$hovedområdekode %in% c(0,3,4,5,6,7))){
     stop("Contains area codes not covered by this covariate assignment.")  
   }
@@ -51,7 +38,6 @@ makeLogbookAdjuster <- function(projectname, logbookfile){
   }
   return(landingsAdjuster)
 }
-
 
 
 projectname <- "Kysttorsk_AFWG_2018"
@@ -75,10 +61,26 @@ adjuster <- function(landings){
 # chosen landingsAdjuster must correspond to namings in the stratafile in the StoX-Reca project
 Rstox::prepareRECA(projectname, minage = 1, maxage = 20, maxlength = 240, landingAdjuster = adjuster)
 saveProjectData(projectname)
-Rstox::runRECA(projectname, burnin = 5000, thin=1, nSamples = 500, CC = T)
+
+
+#sett feillesingsmatrise for kysttorsk-skrei
+#CCerrorList <- list(ptype1.CC=1, ptype2.CC=0.7, ptype4.CC=0.3, ptype5.CC=0, ptype1.S=0, ptype2.S=0.3, ptype4.S=0.7, ptype5.S=1)
+
+l<-loadProjectData(projectname)
+var <- l$prepareRECA
+var$AgeLength$CCerrorList <- CCerrorList
+setProjectData(
+  projectName = projectname,
+  var = var,
+  name = "prepareRECA"
+)
+saveProjectData(projectname)
+
+Rstox::runRECA(projectname, burnin = 5000, thin=1, nSamples = 500, CC = T, CCError = F)
 saveProjectData(projectname)
 
 # plots and reports can now be generated in StoX, or with the following lines of code:
 Rstox::plotRECA(projectname)
 Rstox::reportRECA(projectname)
+
 
