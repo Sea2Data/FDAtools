2e564837e01ac40a555e5f65a6347a12e89ca5e5
edvinf <edvin.fuglebakk@gmail.com>
2020-05-12 15:35:59 +0200

    Made filters species spesific. Improved export

--- dataAcess/countSamples/countSamples.R
+++ dataAcess/countSamples/countSamples.R
@@ -127,6 +127,17 @@ countSamples <- function(individuals, totalLanded, agedBySampleType=F, agesample
     message(paste0(individuals$startyear[1], ": ", length(unique(individuals$serialnumber[is.na(individuals$catchplatform)])), " catches without catchplatform."))
   }
 
+  if (totalLanded > 0){
+    lengthsamples.pr.1000.t = length(unique(individuals$serialnumber)) * 1000 / totalLanded
+    agesamples.pr.1000.t = length(unique(agedindividuals$serialnumber)) * 1000 / totalLanded
+    aged.inidividuals.pr.1000.t = nrow(agedindividuals) * 1000 / totalLanded
+  }
+  else{
+    lengthsamples.pr.1000.t = NA
+    agesamples.pr.1000.t = NA
+    aged.inidividuals.pr.1000.t = NA
+  }
+  
   row <- data.table::data.table(year = individuals$startyear[1],
                                 
                                 no.unique.vessels = length(unique(individuals$catchplatform)),
@@ -138,29 +149,26 @@ countSamples <- function(individuals, totalLanded, agedBySampleType=F, agesample
                                 no.aged.individuals = nrow(agedindividuals),
                                 
                                 landings.tonnes = totalLanded,
-                                lengthsamples.pr.1000.t = length(unique(individuals$serialnumber)) * 1000 / totalLanded,
-                                agesamples.pr.1000.t = length(unique(agedindividuals$serialnumber)) * 1000 / totalLanded,
-                                aged.inidividuals.pr.1000.t = nrow(agedindividuals) * 1000 / totalLanded
+                                lengthsamples.pr.1000.t = lengthsamples.pr.1000.t,
+                                agesamples.pr.1000.t = agesamples.pr.1000.t,
+                                aged.inidividuals.pr.1000.t = aged.inidividuals.pr.1000.t
                                 
                                 )
   return(row)
 }
 
-#' Sampling overview AFWG
+#' Sampling overview across years
 #' @description
 #'  Creates data table with overview of sampling activity for AFWG
 #' @param samplefiles list mapping years to sample file locations (biotic)
 #' @param landingsfiles list mapping years to landing file locations (LSS)
 #' @param speciesSamples species codes to extract from samples (biotic: catchsamples/catchcategory)
 #' @param speciesLanding species codes to extract from landings (lss: ART - FAO kode)
+#' @param areas area codes to extract data for
 #' @param missiontypes mission types to include from samples.
 #' @param addAreas logical, whether to add areas based on position
 #' @return \code{\link[data.table]{data.table}} with one row for each year, formatted as output from ~\code{\link{countSamples}}
-makeAFWGtable <- function(samplefiles, landingsfiles, speciesSamples, speciesLanding, missiontypes=c(1,2,3,9,10,11,12,13,19), addAreas=T){
-  message(paste("Extracting data for mission types:", paste(missiontypes, collapse = ",")))
-  
-  message("Includes Jan-Mayen zone.")
-  areas <- c("00","01","02","03","04","05","06","07","10","11","12","13","14","15","16","17","18","20","21","22","23","24","25","26","27","30","34","35","36","37","38","39","50")
+makesampleCountTable <- function(samplefiles, landingsfiles, speciesSamples, speciesLanding, areas, missiontypes, addAreas=T){
   
   filterBiotic <- function(samples){return(samples[!is.na(samples$system) & samples$system==2 & as.integer(samples$area) %in% as.integer(areas),])}
   filterLanding <- function(landings){return(landings[landings$`Fartøynasjonalitet (kode)`=="NOR" & landings$`Redskap (kode)` != 90 & landings$`Hovedområde (kode)` %in% areas,])}
@@ -193,10 +201,16 @@ makeAFWGtable <- function(samplefiles, landingsfiles, speciesSamples, speciesLan
     
   }
   
+  tab$filterMissiontypes <- paste(sort(missiontypes), collapse=",")
+  tab$filterAreas <- paste(sort(areas), collapse=",")
+  tab$filterSpeciesTsn <- paste(speciesSamples, collapse=",")
+  tab$filterSpeciesFAO <- paste(speciesLanding, collapse=",")
+  
   return(tab)
 }
 
-run <- function(){
+
+saveAFWGtable <- function(outfile="sampleCount.xlsx"){
   samplefiles <- list()
   samplefiles[["2016"]] <- "~/bioticsets/v3/biotic_year_2016.xml"
   samplefiles[["2017"]] <- "~/bioticsets/v3/biotic_year_2017.xml"
@@ -209,11 +223,25 @@ run <- function(){
   landingfiles[["2018"]] <- "~/landingsets/LSS/FDIR_HI_LSS_FANGST_2018_PR_2019-10-03.psv"
   landingfiles[["2019"]] <- "~/landingsets/LSS/FDIR_HI_LSS_FANGST_2019_PR_2020-03-03.psv"
   
+  missiontypes <- c(1,2,3,9,10,11,12,13,19)
+  message(paste("Extracting data for mission types:", paste(missiontypes, collapse = ",")))
   
-  #COD
-  print(makeAFWGtable(samplefiles, landingfiles, speciesSamples = c("164712"), speciesLanding = c("COD")))
+  message("Extracting data for ICES subarea 1 and 2.")
+  message("Excluding area 35,36, and 38 (approximate Jan Mayen zone)")
+  defaultareas <- c("00","01","02","03","04","05","06","07","10","11","12","13","14","15","16","17","18","20","21","22","23","24","25","26","27","30","34","37","39","50")
+  message("CAP: also excluding area 30,34,50 (approximating ICES 2a, west of 5 deg W.)")  
+  capelinareas <- defaultareas[defaultareas != c("30","34","50")]
   
+  
+  tab <- NULL
+
   #CAP
-  print(makeAFWGtable(samplefiles, landingfiles, speciesSamples = c("162035"), speciesLanding = c("CAP")))
+  tab <- rbind(makesampleCountTable(samplefiles, landingfiles, speciesSamples = c("162035"), speciesLanding = c("CAP"), areas = capelinareas, missiontypes = missiontypes), tab)
+    
+  print(tab)
+  #COD
+  tab <- rbind(makesampleCountTable(samplefiles, landingfiles, speciesSamples = c("164712"), speciesLanding = c("COD"), areas = defaultareas, missiontypes = missiontypes), tab)
   
+  print(tab)
+  xlsx::write.xlsx2(tab, outfile, sheetName="sample count", row.names = F)
 }
