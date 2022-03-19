96fbfbcbb08ca389d254f25864960baa9d13ac79
edvinf <edvin.fuglebakk@gmail.com>
2022-03-20 00:05:52 +0100

    fixed issue with area-codes

--- stoxReca/stox2_7_preprocessing/stox2.7preprocessing/R/coastalCodArea.R
+++ stoxReca/stox2_7_preprocessing/stox2.7preprocessing/R/coastalCodArea.R
@@ -37,7 +37,7 @@ encodeCostalCodArea <- function(landings){
   if (!is.null(landings$FOlat)){
     # set area from position when available
     landingsWpos <- RstoxFDA::appendAreaCode(landingsWpos, stox2.7preprocessing::coastalCodAreas, "FOlat", "FOlon", "areaFromPos", StratumName = "polygonName")
-    landingsWpos[["Hovedomr\u00E5de_kode"]] <- landingsWpos$areaFromPos
+    landingsWpos[["Hovedomr\u00E5de_kode"]][landingsWpos$areaFromPos %in% c("s300", "s400", "s500", "s600", "s700", "s000", "s301", "s401", "s501", "s601", "s701")] <- landingsWpos$areaFromPos[landingsWpos$areaFromPos %in% c("s300", "s400", "s500", "s600", "s700", "s000", "s301", "s401", "s501", "s601", "s701")]
     landingsWpos$areaFromPos <- NULL
     landings <- rbind(landings, landingsWpos)
   }
--- stoxReca/stox2_7_preprocessing/stox2.7preprocessing/inst/examplescript/example.R
+++ stoxReca/stox2_7_preprocessing/stox2.7preprocessing/inst/examplescript/example.R
@@ -1,20 +1,21 @@
 library(stox2.7preprocessing)
 
 # prepare data for coastal cod estimate 2021 (seasonal conversion factors):
-processLandingsAllAdjustmentsAFWG("~/temp/torsk_2021_logbook_adjusted.xml",
-                                  "~/landingsets/xml/filtered/landing-year-2021-species-1022.xml",
-                                  "~/logbooks/FDIR_HI_ERS_2021_PR_2021-12-02.psv", coastalCod=T)
+processLandingsAllAdjustmentsAFWG("~/temp/torsk_kyst_2021_logbook_and_factor_adjusted_pr_mars_2022.xml",
+                                  "~/landingsets/xml/filtered/torsk_2021_pr_mars_2022.xml",
+                                  "~/logbooks/FDIR_HI_ERS_2021_PR_2022-03-04.psv",
+                                  seasonalConversionFactor=T, coastalCod=T)
 
 # prepare data for NEA cod estimate in coastal region (without seasonal conversion factors):
-processLandingsAllAdjustmentsAFWG("~/temp/kysttorsk_2021_logbook_and_factor_adjusted.xml",
-                                  "~/landingsets/xml/filtered/landing-year-2021-species-1022.xml",
-                                  "~/logbooks/FDIR_HI_ERS_2021_PR_2021-12-02.psv",
+processLandingsAllAdjustmentsAFWG("~/temp/torsk_kyst_2021_logbook_adjusted_pr_mars_2022.xml",
+                                  "~/landingsets/xml/filtered/torsk_2021_pr_mars_2022.xml",
+                                  "~/logbooks/FDIR_HI_ERS_2021_PR_2022-03-04.psv",
                                   seasonalConversionFactor=F, coastalCod=T)
 
 # prepare data for NEA cod estimate in offshore region (without seasonal conversion factors):
-processLandingsAllAdjustmentsAFWG("~/temp/kysttorsk_2021_logbook_and_factor_adjusted.xml",
-                                  "~/landingsets/xml/filtered/landing-year-2021-species-1022.xml",
-                                  "~/logbooks/FDIR_HI_ERS_2021_PR_2021-12-02.psv",
+processLandingsAllAdjustmentsAFWG("~/temp/torsk_hav_2021_logbook_adjusted.xml",
+                                  "~/landingsets/xml/filtered/torsk_2021_pr_mars_2022.xml",
+                                  "~/logbooks/FDIR_HI_ERS_2021_PR_2022-03-04.psv",
                                   seasonalConversionFactor=F, coastalCod=F)
 
 # get strata files for Stox2.7 DefineStrata
