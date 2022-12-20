122715fa432d6a9ab1b54b24dba31885f277ae3c
edvinf <edvin.fuglebakk@gmail.com>
2022-12-20 10:14:03 +0100

    Updated example for 2022

--- stoxReca/stox2_7_preprocessing/stox2.7preprocessing/inst/examplescript/example.R
+++ stoxReca/stox2_7_preprocessing/stox2.7preprocessing/inst/examplescript/example.R
@@ -1,21 +1,21 @@
 library(stox2.7preprocessing)
 
 # prepare data for coastal cod estimate 2021 (seasonal conversion factors):
-processLandingsAllAdjustmentsAFWG("~/temp/torsk_kyst_2021_logbook_and_factor_adjusted_pr_mars_2022.xml",
-                                  "~/landingsets/xml/filtered/torsk_2021_pr_mars_2022.xml",
-                                  "~/logbooks/FDIR_HI_ERS_2021_PR_2022-03-04.psv",
+processLandingsAllAdjustmentsAFWG("~/temp/torsk_kyst_2022_logbook_and_factor_adjusted_pr_des_2022.xml",
+                                  "~/landingsets/xml/filtered/torsk_2022_pr_des_2022.xml",
+                                  "~/logbooks/FDIR_HI_ERS_2022_PR_2022-12-12.psv",
                                   seasonalConversionFactor=T, coastalCod=T)
 
 # prepare data for NEA cod estimate in coastal region (without seasonal conversion factors):
-processLandingsAllAdjustmentsAFWG("~/temp/torsk_kyst_2021_logbook_adjusted_pr_mars_2022.xml",
-                                  "~/landingsets/xml/filtered/torsk_2021_pr_mars_2022.xml",
-                                  "~/logbooks/FDIR_HI_ERS_2021_PR_2022-03-04.psv",
+processLandingsAllAdjustmentsAFWG("~/temp/torsk_kyst_2022_logbook_adjusted_pr_des_2022.xml",
+                                  "~/landingsets/xml/filtered/torsk_2022_pr_des_2022.xml",
+                                  "~/logbooks/FDIR_HI_ERS_2022_PR_2022-12-12.psv",
                                   seasonalConversionFactor=F, coastalCod=T)
 
 # prepare data for NEA cod estimate in offshore region (without seasonal conversion factors):
-processLandingsAllAdjustmentsAFWG("~/temp/torsk_hav_2021_logbook_adjusted.xml",
-                                  "~/landingsets/xml/filtered/torsk_2021_pr_mars_2022.xml",
-                                  "~/logbooks/FDIR_HI_ERS_2021_PR_2022-03-04.psv",
+processLandingsAllAdjustmentsAFWG("~/temp/torsk_hav_2022_logbook_adjusted.xml",
+                                  "~/landingsets/xml/filtered/torsk_2022_pr_des_2022.xml",
+                                  "~/logbooks/FDIR_HI_ERS_2022_PR_2022-12-12.psv",
                                   seasonalConversionFactor=F, coastalCod=F)
 
 # get strata files for Stox2.7 DefineStrata
